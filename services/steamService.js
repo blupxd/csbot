3110d62ba9f484b82c82f52c5dce88697044a82b
Matija Stefanovic <ms20230257@student.fon.bg.ac.rs>
2025-01-23 16:39:55 +0100

    refresh Token

--- services/steamService.js
+++ services/steamService.js
@@ -1,17 +1,21 @@
 const SteamUser = require("steam-user");
 const { loadToken, saveToken } = require("./database");
 
-const client = new SteamUser();
+const client = new SteamUser({
+  rememberPassword: true,
+  renewRefreshTokens: true
+});
 let pendingSteamGuardCallback = null;
 
 async function logInSteam() {
-  const sentry = await loadToken();
-  const logOnOptions = {
-    accountName: process.env.STEAM_USERNAME,
-    password: process.env.STEAM_PASSWORD,
-    rememberPassword: true,
-    sentry,
-  };
+  const refreshToken = await loadToken();
+  const logOnOptions = refreshToken
+    ? { refreshToken } // Koristi postojeći refresh token ako postoji
+    : {
+        accountName: process.env.STEAM_USERNAME,
+        password: process.env.STEAM_PASSWORD,
+        rememberPassword: true,
+      };
 
   client.logOn(logOnOptions);
 
@@ -28,17 +32,25 @@ async function logInSteam() {
     pendingSteamGuardCallback = callback; // Spremamo callback za kasniju upotrebu
   });
 
-  client.on("machineAuthToken", (machineAuth) => {
-    saveToken(machineAuth);
+  client.on("refreshToken", (token) => {
+    saveToken(token)
+      .then(() => console.log("Refresh token uspešno sačuvan u MongoDB!"))
+      .catch((err) =>
+        console.error("Greška pri čuvanju refresh tokena:", err.message)
+      );
   });
 
   client.on("error", (err) => {
     console.error("Došlo je do greške:", err.message);
     if (err.eresult === SteamUser.EResult.InvalidPassword) {
-      console.log("Sesija je istekla, pokušavamo ponovo prijaviti korisnika...");
+      console.log(
+        "Sesija je istekla, pokušavamo ponovo prijaviti korisnika..."
+      );
       logInSteam();
     } else if (err.eresult === SteamUser.EResult.LoggedInElsewhere) {
-      console.log("Korisnik je prijavljen na drugom uređaju, pokušavamo ponovo...");
+      console.log(
+        "Korisnik je prijavljen na drugom uređaju, pokušavamo ponovo..."
+      );
       logInSteam();
     } else {
       console.log("Pokušavamo ponovo prijaviti korisnika za 5 sekundi...");
@@ -47,7 +59,9 @@ async function logInSteam() {
   });
 
   client.on("disconnected", (eresult, msg) => {
-    console.log(`Korisnik je odjavljen: ${msg}. Pokušavamo ponovo prijaviti korisnika za 5 sekundi...`);
+    console.log(
+      `Korisnik je odjavljen: ${msg}. Pokušavamo ponovo prijaviti korisnika za 5 sekundi...`
+    );
     setTimeout(logInSteam, 5000);
   });
 }
